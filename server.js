bd4850775841a440b9e1ed867fda6c4d7b2daaa4
Jonathan Vega <jonnyv212@hotmail.com>
2019-07-09 23:47:52 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const PORT = process.env.PORT || 5000;
+const { Client } = require("pg");
+
+const connection = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, "client/build")));
+
+connection.connect(err => {
+  //  console.log(process.env);
+  //  console.log(process.env.DATABASE_URL);
+  if (err) {
+    return err;
+  }
+});
+
+app.get("/db", (req, res) => {
+  connection.query("SELECT * FROM public.skills", (err, results) => {
+    if (err) {
+      return res.send(err);
+    } else {
+      // res.json({ data: results.rows });
+      res.send(results.rows);
+      // res.send("Hey, we got here");
+      console.log("results: " + results.rows);
+    }
+  });
+});
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening to port: ${PORT}`);
+});
